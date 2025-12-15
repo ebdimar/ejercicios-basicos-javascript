49b0951c676f1e8f27322fa94c23148ba9cd56e5
ivanmartinez777 <ivanmartinez777@hotmail.com>
2025-12-15 11:07:13 +0100

    feat: Exercici 1, these group of exercises is about arrow functions

---
+++ exercici1-1.js
@@ -0,0 +1,74 @@
+/*
+Exercici 1: Conversió de funcions: Tens una funció add que accepta dos paràmetres i
+            retorna la seva suma. Converteix-la en una funció de fletxa. Per exemple:
+             function add(a, b) {return a + b;}
+*/
+
+const add = (a, b) => a + b;
+
+console.log("Prueba del ejercicio 1:", add(9, 8));
+
+/* 
+Exercici 2: Funció de fletxa sense paràmetres: Crea una funció de fletxa anomenada
+                randomNumber que no necessiti paràmetres i que retorni un número aleatori 
+                entre 0 i 100.
+*/
+const randomNumber = () => Math.floor(Math.random() * (100 - 1) + 1);
+
+//Prueba
+console.log("Prueba del ejercicio 2. El número aleatorio es: ", randomNumber());
+
+/*
+Exercici 3: Ús de 'this' en les funcions de fletxa: Crea una classe person que tingui una
+            propietat name i una funció greet que utilitzi una funció de fletxa. La funció
+            ha d'imprimir una salutació que inclogui el nom de la persona. Per exemple: 
+            console.log(Hola, ${this.name});.
+ */
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+  greet = () => {
+    console.log(`Hola, ${this.name}`);
+  };
+}
+
+const person = new Person("Miguel");
+person.greet();
+
+/*
+Exercici 4: Funció de fletxa dins d'un loop: Crea una funció anomenada printNumbers que
+            accepti un array de números i utilitzi un loop for per imprimir cada número 
+            a la consola utilitzant una funció de fletxa.
+*/
+
+const printNumbers = (numberArray) => {
+  if (Array.isArray(numberArray) === true) {
+    const print = (number) => {
+      if (typeof number === "number") {
+        console.log(number);
+      } else {
+        console.log("Este elemento del array no es un número");
+      }
+    };
+    for (let i = 0; i < numberArray.length; i++) {
+      print(numberArray[i]);
+    }
+  } else {
+    console.log("El argumento de la función no es un array");
+  }
+};
+
+printNumbers([1, 2, 3, 5, 7, 9, 6, 2, "asdf", null]);
+
+/* 
+Exercici 5: Funció de fletxa amb 'setTimeout': Crea una funció de fletxa que imprimeixi un 
+            missatge a la consola després d'esperar 3 segons. */
+
+const delayedMessage = () => {
+  setTimeout(() => {
+    console.log("Prueba del ejercicio 5 que se imprime después de 3 segundos");
+  }, "3000");
+};
+
+delayedMessage();
